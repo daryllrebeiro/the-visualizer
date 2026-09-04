b7a591259fcde70d3b7705b61e11c704d7cbdf49
daryllrebeiro <daryllrebeiro07@gmail.com>
2026-09-04 16:05:37 +0530

    fix(ci): format vitest.config.ts and add db:migrate steps

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
     env: {
       NODE_ENV: 'test',
       DATABASE_URL:
-        process.env['DATABASE_URL'] || 'postgresql://visualizer:visualizer_test@localhost:5432/visualizer_test',
+        process.env['DATABASE_URL'] ||
+        'postgresql://visualizer:visualizer_test@localhost:5432/visualizer_test',
       SESSION_SECRET: 'test_session_secret_at_least_32_characters_long_123456',
       REDIS_URL: 'redis://localhost:6379',
       JWT_SECRET: 'test_jwt_secret_at_least_32_characters_long_123456',
